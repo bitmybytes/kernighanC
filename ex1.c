d7d7c33d3ee4d756edc6f2268dca539e550fa9c8
arunsk <arunsk.tec@gmail.com>
2013-10-20 13:07:06 -0400

    updated with right-justified

--- ex1.c
+++ ex1.c
@@ -16,7 +16,7 @@ int main()
     while(fahr <= upper) {
 
         celsius = 5 * (fahr - 32) / 9 ;
-        printf("%d\t%d\n", fahr, celsius);
+        printf("%3d\t%6d\n", fahr, celsius);
         fahr = fahr + step;
     }
 
