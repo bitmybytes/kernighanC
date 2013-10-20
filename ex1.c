6efc445854ba68e19a2cc00ba36c635982b6e63a
arunsk <arunsk.tec@gmail.com>
2013-10-20 14:17:00 -0400

    modified to print a heading table

--- ex1.c
+++ ex1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* print Farenheit-Celcius table
+/* print Farenheit-Celsius table
  * for fahr = 0, 20, ..., 300 ; floating-point version */
 
 int main() 
@@ -13,10 +13,14 @@ int main()
     step  = 20; /* step size */
 
     fahr = lower;
+
+    printf("%s | %s\n", "Celsius", "Farenheit");
+    printf("====================\n");
+
     while(fahr <= upper) {
 
         celsius = (5.0/9.0) * (fahr - 32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
+        printf("%3.0f %13.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 
